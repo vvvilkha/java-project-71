d8a4ac0b4e22a6d1aa9ca29d4ccf6b3fbc5975a0
vilkha <vilkha34@gmail.com>
2025-07-09 12:32:48 +0300

    4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 @Command(
         name = "gendiff",
@@ -11,9 +13,21 @@ import picocli.CommandLine.Command;
 )
 public class App implements Runnable {
 
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
     @Override
     public void run() {
-        
+        // Пока просто выводим, что считали аргументы
+        System.out.println("File 1: " + filepath1);
+        System.out.println("File 2: " + filepath2);
+        System.out.println("Format: " + format);
     }
 
     public static void main(String[] args) {
